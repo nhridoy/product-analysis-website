7a8f74f4f3007bef2fd63322b5d461d815fa191c
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-04 01:36:18 +0600

    Review Page Responsive

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -5,12 +5,12 @@ import useReviews from "../../hooks/useReviews";
 const Reviews = () => {
   const [reviews, setReviews] = useReviews();
   return (
-    <div className="py-4 bg-white">
+    <div className="p-4 bg-white">
       <div className="container mx-auto flex flex-col gap-4 items-center">
         <h2 className="text-2xl font-semibold text-gray-700">
           What Our Customer Says
         </h2>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {reviews.map((review) => (
             <SingleReview key={review.id} review={review} />
           ))}

215f2d16157cacde03b7dc594133e121d1544061
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-03 23:08:25 +0600

    Head Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="container mx-auto p-4 sticky top-4">
+      <div className="flex justify-between items-center">
+        <NavLink
+          to="/"
+          className="border-4 rounded border-orange-500 text-3xl font-semibold px-4 py-3"
+        >
+          <span className="text-gray-700">Eye Watch</span>
+        </NavLink>
+
+        <div className="flex flex-row gap-4 text-gray-700">
+          <NavLink to="/" className="border-gray-300 border-b-2">
+            Home
+          </NavLink>
+
+          <NavLink to="dashboard" className="border-gray-300 border-b-2">
+            Dashboard
+          </NavLink>
+
+          <NavLink to="reviews" className="border-gray-300 border-b-2">
+            Reviews
+          </NavLink>
+
+          <NavLink to="blogs" className="border-gray-300 border-b-2">
+            Blogs
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
